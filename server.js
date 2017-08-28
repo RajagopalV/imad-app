1dfd79fa32e075d888ba3b83332bdce4b27c750b
RajagopalV <30712215+RajagopalV@users.noreply.github.com>
2017-08-28 10:02:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -140,7 +140,7 @@ app.get('/testDb', function (req, res) {
     console.log('Inside test db');
     pool.query("SELECT * FROM article WHERE titile=article1",function(err,result){
         if(err){
-            res.status(500).send(err.toString);
+            res.status(500).send(err.toString());
         }else{
             res.send(JSON.stringify(result.rows));
         }
@@ -156,7 +156,7 @@ app.get('/article/:articleName', function (req, res) {
     console.log('url requested : '+req.params.articleName );
     pool.query('SELECT * FROM article WHERE title = '+ req.params.articleName,function(err,result){
         if(err){
-            res.status(500).send(err.toString);
+            res.status(500).send(err.toString());
         }else{
             
             if(result.rows.length === 0){
