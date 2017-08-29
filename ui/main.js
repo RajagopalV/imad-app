a1622687b0486cafc2a34f3f67675856775ae2b3
RajagopalV <30712215+RajagopalV@users.noreply.github.com>
2017-08-29 10:03:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,40 +1,4 @@
 console.log("Loaded!!");
-var button = document.getElementById('commentButton');
-
-button.onclick = function(){
-	
-	//Get comment
-	var commentText = document.getElementById('commentText');
-	var comment = commentText.value;
-	
-	
-	//http request
-	
-	var httpRequest = new XMLHttpRequest();
-	
-	httpRequest.onreadystatechange = function(){
-		if(httpRequest.readyState == XMLHttpRequest.DONE){
-			//response received
-			if(httpRequest.status == 200){
-				var comments = httpRequest.responseText;
-				comments = JSON.parse(comments);
-				var list='';
-				
-				for(var i=0;i<comments.length;i++){
-					list += '<li>' + comments[i] + '</li><br><hr>';
-				}
-				var ul = document.getElementById('commentList');
-				ul.innerHTML = list;
-				
-			}
-		}
-	}
-	
-	//Make request
-	httpRequest.open('GET','http://gopalequal.imad.hasura-app.io/submit-comment?comment='+comment,true);
-	httpRequest.send(null);
-	
-}
 
 var button1 = document.getElementById('submit_btn');
 
@@ -65,4 +29,43 @@ button1.onclick = function(){
 	httpRequest.send(JSON.stringify({username:username , password:password}));
 	
 	
+}
+
+var button = document.getElementById('commentButton');
+
+if(button !== null){
+    button.onclick = function(){
+    	
+    	//Get comment
+    	var commentText = document.getElementById('commentText');
+    	var comment = commentText.value;
+    	
+    	
+    	//http request
+    	
+    	var httpRequest = new XMLHttpRequest();
+    	
+    	httpRequest.onreadystatechange = function(){
+    		if(httpRequest.readyState == XMLHttpRequest.DONE){
+    			//response received
+    			if(httpRequest.status == 200){
+    				var comments = httpRequest.responseText;
+    				comments = JSON.parse(comments);
+    				var list='';
+    				
+    				for(var i=0;i<comments.length;i++){
+    					list += '<li>' + comments[i] + '</li><br><hr>';
+    				}
+    				var ul = document.getElementById('commentList');
+    				ul.innerHTML = list;
+    				
+    			}
+    		}
+    	}
+    	
+    	//Make request
+    	httpRequest.open('GET','http://gopalequal.imad.hasura-app.io/submit-comment?comment='+comment,true);
+    	httpRequest.send(null);
+    	
+    }
 }
