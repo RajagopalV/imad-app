c4193dd02ec6459641fd54b0cef78cf5f713a14b
RajagopalV <30712215+RajagopalV@users.noreply.github.com>
2017-08-30 10:42:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -257,7 +257,9 @@ app.get('/article/:articleName', function (req, res) {
             if(result.rows.length === 0){
                  res.status(404).send("Article not found!!!");
             }else{
-                 //req.session.auth.articleId = result.rows[0].id;
+                if(req.session && re.session.auth){    
+                 req.session.auth.articleId = result.rows[0].id;
+                }
                 res.send(createTemplate(result.rows));
             }
         }
